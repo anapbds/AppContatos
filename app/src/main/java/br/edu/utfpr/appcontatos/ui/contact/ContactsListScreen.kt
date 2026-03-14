715b4f17d1303d4e1044cf05e54cae4105b5048a
anapbds <justtley@gmail.com>
2026-03-14 14:10:30 -0300

    Componente de lista

--- app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/ContactsListScreen.kt
+++ app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/ContactsListScreen.kt
@@ -1,5 +1,6 @@
 package br.edu.utfpr.appcontatos.ui.contact
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,26 +9,61 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CloudOff
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.edu.utfpr.appcontatos.R
+import br.edu.utfpr.appcontatos.data.Contact
 import br.edu.utfpr.appcontatos.ui.theme.AppContatosTheme
+import kotlin.random.Random
 
 @Composable
 fun ContactsListScreen(modifier: Modifier = Modifier) {
+    var isLoading = true
+    var isError = false
+    var contacts = listOf<Contact>()
 
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = { AppBar() }
+    ) { paddingValues ->
+        val defaultModifier: Modifier = Modifier.padding(paddingValues)
+        if (isLoading) {
+            LoadingState()
+        }
+        else if (isError) {
+            ErrorState()
+        }
+        else if (contacts.isEmpty()) {
+            EmptyList()
+        }
+        else {
+            List(contacts = contacts)
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -126,4 +162,123 @@ fun ErrorStatePreview() {
     AppContatosTheme {
         ErrorState()
     }
+}
+
+@Composable
+fun EmptyList(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(all = 16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.no_data),
+            contentDescription = "Nada por aqui..."
+        )
+        Text(
+            text = "Nada por aqui...",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Você ainda não adicionou nenhum contato. " +
+                    "\nAdicione o primeiro utilizando o botão \"Novo contato\"",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+private fun EmptyListPreview() {
+    AppContatosTheme {
+        EmptyList()
+    }
+}
+
+@Composable
+fun List(
+    modifier: Modifier = Modifier,
+    contacts: List<Contact> = emptyList()
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(state = rememberScrollState())
+    ) {
+        contacts.forEach { contact ->
+            var isFavorite = contact.isFavorite
+            ListItem(
+                headlineContent = {
+                    Text(contact.fullName)
+                },
+                trailingContent = {
+                    IconButton(
+                        onClick = {
+                            isFavorite = !isFavorite
+                        }
+                    ) {
+                        Icon (
+                            imageVector = if (isFavorite) {
+                                Icons.Filled.Favorite
+                            } else {
+                                Icons.Filled.FavoriteBorder
+                            },
+                            contentDescription = "Favoritar",
+                            tint = if (isFavorite) {
+                                Color.Red
+                            } else {
+                                LocalContentColor.current
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ListPreview() {
+    AppContatosTheme {
+        List(
+            contacts = generateContacts()
+        )
+    }
+}
+
+private fun generateContacts(): List<Contact> {
+    val firstNames = listOf(
+        "João", "José", "Everton", "Marcos", "André", "Anderson", "Antônio",
+        "Laura", "Ana", "Maria", "Joaquina", "Suelen"
+    )
+    val lastNames = listOf(
+        "Do Carmo", "Oliveira", "Dos Santos", "Da Silva", "Brasil", "Pichetti",
+        "Cordeiro", "Silveira", "Andrades", "Cardoso"
+    )
+    val contacts: MutableList<Contact> = mutableListOf()
+    for (i in 0..19) {
+        var generatedNewContact = false
+        while (!generatedNewContact) {
+            val firstNameIndex = Random.nextInt(firstNames.size)
+            val lastNameIndex = Random.nextInt(lastNames.size)
+            val newContact = Contact(
+                id = i + 1,
+                firstName = firstNames[firstNameIndex],
+                lastName = lastNames[lastNameIndex]
+            )
+            if (!contacts.any { it.fullName == newContact.fullName }) {
+                contacts.add(newContact)
+                generatedNewContact = true
+            }
+        }
+    }
+    return contacts
 }
