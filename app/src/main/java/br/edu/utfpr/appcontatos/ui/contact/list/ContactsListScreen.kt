d56613c279df68fab48b9d3f4e4cd1e887873865
anapbds <justtley@gmail.com>
2026-03-21 15:39:25 -0300

    Aula 21/03

---
+++ app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/list/ContactsListScreen.kt
@@ -0,0 +1,248 @@
+package br.edu.utfpr.appcontatos.ui.contact.list
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import br.edu.utfpr.appcontatos.R
+import br.edu.utfpr.appcontatos.data.Contact
+import br.edu.utfpr.appcontatos.data.groupByInitial
+import br.edu.utfpr.appcontatos.ui.shared.composables.ContactAvatar
+import br.edu.utfpr.appcontatos.ui.shared.composables.DefaultErrorState
+import br.edu.utfpr.appcontatos.ui.shared.composables.DefaultLoadingState
+import br.edu.utfpr.appcontatos.ui.shared.composables.FavoriteIconButton
+import br.edu.utfpr.appcontatos.ui.theme.AppContatosTheme
+import br.edu.utfpr.appcontatos.data.utils.generateContacts
+
+@Composable
+fun ContactsListScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ContactsListViewModel = viewModel(),
+    onAddPressed: () -> Unit,
+    onContactPressed: (Contact) -> Unit
+) {
+    val contentModifier = modifier.fillMaxSize()
+    if (viewModel.uiState.isLoading) {
+        DefaultLoadingState(
+            modifier = contentModifier,
+            loadingMessage = "Carregando contatos..."
+        )
+    } else if (viewModel.uiState.hasError) {
+        DefaultErrorState(
+            modifier = contentModifier,
+            onTryAgainPressed = viewModel::loadContacts
+        )
+    } else {
+        Scaffold(
+            modifier = modifier.fillMaxSize(),
+            topBar = {
+                AppBar(
+                    onRefreshPressed = viewModel::loadContacts
+                )
+            },
+            floatingActionButton = {
+                ExtendedFloatingActionButton(onClick = onAddPressed) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Adicionar"
+                    )
+                    Spacer(Modifier.size(8.dp))
+                    Text("Novo contato")
+                }
+            }
+        ) { paddingValues ->
+            val defaultModifier: Modifier = Modifier.padding(paddingValues)
+            if (viewModel.uiState.contacts.isEmpty()) {
+                EmptyList(modifier = defaultModifier)
+            } else {
+                List(
+                    modifier = defaultModifier,
+                    contacts = viewModel.uiState.contacts,
+                    onFavoritePressed = viewModel::toggleIsFavorite,
+                    onContactPressed = onContactPressed
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    modifier: Modifier = Modifier,
+    onRefreshPressed: () -> Unit
+) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Text("Contatos")
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            titleContentColor = MaterialTheme.colorScheme.primary,
+            actionIconContentColor = MaterialTheme.colorScheme.primary
+        ),
+        actions = {
+            IconButton(onClick = onRefreshPressed) {
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = "Atualizar"
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AppBarPreview() {
+    AppContatosTheme {
+        AppBar(
+            onRefreshPressed = {}
+        )
+    }
+}
+
+@Composable
+fun EmptyList(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(all = 16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.no_data),
+            contentDescription = "Nada por aqui..."
+        )
+        Text(
+            text = "Nada por aqui...",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Você ainda não adicionou nenhum contato." +
+                    "\nAdicione o primeiro utilizando o botão \"Novo contato\"",
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+fun EmptyListPreview() {
+    AppContatosTheme {
+        EmptyList()
+    }
+}
+
+@Composable
+fun List(
+    modifier: Modifier = Modifier,
+    contacts: Map<String, List<Contact>> = emptyMap(),
+    onFavoritePressed: (Contact) -> Unit,
+    onContactPressed: (Contact) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        contacts.forEach { (key, contacts) ->
+            stickyHeader {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                ) {
+                    Text(
+                        text = key,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp, bottom = 8.dp, start = 16.dp),
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
+            }
+            items(contacts) { contact ->
+                ContactListItem(
+                    contact = contact,
+                    onFavoritePressed = onFavoritePressed,
+                    onContactPressed = onContactPressed
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListPreview() {
+    AppContatosTheme {
+        List(
+            contacts = generateContacts().groupByInitial(),
+            onFavoritePressed = {},
+            onContactPressed = {}
+        )
+    }
+}
+
+@Composable
+fun ContactListItem(
+    modifier: Modifier = Modifier,
+    contact: Contact,
+    onFavoritePressed: (Contact) -> Unit,
+    onContactPressed: (Contact) -> Unit
+) {
+    ListItem(
+        modifier = modifier.clickable { onContactPressed(contact) },
+        headlineContent = {
+            Text(contact.fullName)
+        },
+        leadingContent = {
+            ContactAvatar(
+                firstName = contact.firstName,
+                lastName = contact.lastName
+            )
+        },
+        trailingContent = {
+            FavoriteIconButton(
+                isFavorite = contact.isFavorite,
+                onPressed = { onFavoritePressed(contact) }
+            )
+        }
+    )
+}
---
+++ app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/list/ContactsListViewModel.kt
@@ -0,0 +1,54 @@
+package br.edu.utfpr.appcontatos.ui.contact.list
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.edu.utfpr.appcontatos.data.Contact
+import br.edu.utfpr.appcontatos.data.ContactDatasource
+import br.edu.utfpr.appcontatos.data.groupByInitial
+import kotlinx.coroutines.launch
+
+class ContactsListViewModel : ViewModel() {
+    var uiState: ContactsListUiState by mutableStateOf(ContactsListUiState())
+        private set
+
+    init {
+        loadContacts()
+    }
+
+    fun loadContacts() {
+        uiState = uiState.copy(
+            isLoading = true,
+            hasError = false
+        )
+        viewModelScope.launch {
+           uiState = try {
+               uiState.copy(
+                   isLoading = false,
+                   contacts = ContactDatasource.instance.findAll().groupByInitial()
+               )
+            } catch (e: Exception) {
+                Log.e("ContactsListViewModel", "Erro ao carregar contatos", e)
+                uiState.copy(
+                    isLoading = false,
+                    hasError = true
+                )
+            }
+        }
+    }
+
+    fun toggleIsFavorite(contact: Contact) {
+        try {
+            val updateContact = contact.copy(isFavorite = !contact.isFavorite)
+            ContactDatasource.instance.save(updateContact)
+            uiState = uiState.copy(
+                contacts = ContactDatasource.instance.findAll().groupByInitial()
+            )
+        } catch (ex: Exception) {
+            Log.e("ContactsListViewModel", "Erro ao atualizar contato", ex)
+        }
+    }
+}
